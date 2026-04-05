a8223b6ee76cbf77d5d0ed986abf788ca86f8c62
parthanallelu <partha060305@gmail.com>
2026-04-05 16:55:08 +0530

    fix: add missing go_router import in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 import 'core/theme/app_theme.dart';
 import 'core/constants/app_constants.dart';
 import 'providers/auth_provider.dart';
